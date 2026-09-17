c83766df47c154d04d94941bb5fe77ded3ef843f
Z-K-Heart <1930750335@qq.com>
2026-09-17 16:13:35 +0800

    Complete strings

--- exercises/strings/strings4.rs
+++ exercises/strings/strings4.rs
@@ -7,8 +7,6 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
-
 fn string_slice(arg: &str) {
     println!("{}", arg);
 }
@@ -17,14 +15,14 @@ fn string(arg: String) {
 }
 
 fn main() {
-    ???("blue");
-    ???("red".to_string());
-    ???(String::from("hi"));
-    ???("rust is fun!".to_owned());
-    ???("nice weather".into());
-    ???(format!("Interpolation {}", "Station"));
-    ???(&String::from("abc")[0..1]);
-    ???("  hello there ".trim());
-    ???("Happy Monday!".to_string().replace("Mon", "Tues"));
-    ???("mY sHiFt KeY iS sTiCkY".to_lowercase());
+    string_slice("blue");
+    string("red".to_string());
+    string(String::from("hi"));
+    string("rust is fun!".to_owned());  //创建一个拥有所有权的副本
+    string_slice("nice weather".into());  //根据目标类型推断
+    string(format!("Interpolation {}", "Station"));  //格式化生成新的字符串
+    string_slice(&String::from("abc")[0..1]);
+    string_slice("  hello there ".trim());  //去除字符串两端空白
+    string("Happy Monday!".to_string().replace("Mon", "Tues"));  //替换字符串中的指定内容
+    string("mY sHiFt KeY iS sTiCkY".to_lowercase());  //转换为小写
 }
